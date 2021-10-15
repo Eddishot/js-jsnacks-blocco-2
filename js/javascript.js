85d274ff4a8e2f05fb0b2ed6f3fb2d0efe800140
Eddishot <Eddi_sl94@hotmail.it>
2021-10-15 16:52:44 +0200

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+const numeroUtente = parseint(prompt("inserisci un numero"));
+
+
+if (numeroUtente % 2== 0 ){
+    alert("Il numero è Pari");
+    console.log("numero pari")
+} else
+{
+ alert("Il numero è Dispari");
+ console.log("numero dispari")
+}
