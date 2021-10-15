7898566fc10fbc76d535fdf9b441f258607adb19
Eddishot <Eddi_sl94@hotmail.it>
2021-10-15 17:05:59 +0200

    commit

---
+++ js/array.js
@@ -0,0 +1,20 @@
+
+
+// creo due array
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+// creo una variabile in cui posso pusshare gli elementi
+
+let addingElemets = array1.push
+
+// creo un let per sapere il numero dell'indice
+let number = array1[0];
+
+let number2 = array2[0];
+
+
+
+for (let i=0; i < array1.lenght; i++){
+    
+}
